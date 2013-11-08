38971fe99c6ede4d0354029be81bb3eb0003bc96
Ken Smith <smithkl42@gmail.com>
2013-11-08 15:23:00 -0800

    Initial commit

---
+++ Payboard.Sdk.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Payboard.Sdk.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
