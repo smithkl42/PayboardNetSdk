cdf65a5a274853885eacc4dc43bfc16d9295e395
Ken Smith <smithkl42@gmail.com>
2013-11-20 15:26:50 -0800

    Code cleaning.

--- Payboard.Sdk.Demo/Program.cs
+++ Payboard.Sdk.Demo/Program.cs
@@ -17,53 +17,51 @@ namespace Payboard.Sdk.Demo
             {
                 // Not used at the moment - just showing how it's done.
                 var lastSynchronizedOn = syncResult.Result ?? DateTime.UtcNow.AddDays(-1);
-
-                //Open connection
-                conn.Open();
-                const string commandText = "SELECT top 5 * from [user] where firstname='Matt'";
-                var command = new SqlCommand(commandText, conn);
-
-                //select items from a database, and var them out
-                var dataReader = command.ExecuteReader();
-
-                //Display the results (if any)
-                var events = new List<CustomerUserEvent>();
-                if (dataReader.HasRows)
-                {
-                    while (dataReader.Read())
-                    {
-                        var @event = new CustomerUserEvent();
-                        @event.CustomerId = dataReader.GetInt32(0).ToString();
-                        @event.CustomerName = dataReader.GetString(2) + " " + dataReader.GetString(3);
-                        @event.CustomerUserId = dataReader.GetInt32(0).ToString();
-                        @event.CustomerUserEmail = dataReader.GetString(4);
-                        @event.CustomerUserFirstName = dataReader.GetString(2);
-                        @event.CustomerUserLastName = dataReader.GetString(3);
-                        @event.EventName = "TestEventRecorded";
-                        events.Add(@event);
-                        //Console.WriteLine("\t{0}\t\t{1}", dataReader.GetString(0), dataReader.GetString(1));
-                    }
-                }
-                else
-                    Console.WriteLine("No rows returned.");
-
-                dataReader.Close();
-                conn.Close();
-
-                service.TrackCustomerUserEvents(events, true).ContinueWith(result =>
-                {
-                    if (result.IsFaulted)
-                    {
-                        Console.WriteLine(result.Exception);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The events were recorded");
-                    }
-                });
-
             });
 
+            //Open connection
+            conn.Open();
+            const string commandText = "SELECT top 5 * from [user] where firstname='Matt'";
+            var command = new SqlCommand(commandText, conn);
+
+            //select items from a database, and var them out
+            var dataReader = command.ExecuteReader();
+
+            //Display the results (if any)
+            var events = new List<CustomerUserEvent>();
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    var @event = new CustomerUserEvent();
+                    @event.CustomerId = dataReader.GetInt32(0).ToString();
+                    @event.CustomerName = dataReader.GetString(2) + " " + dataReader.GetString(3);
+                    @event.CustomerUserId = dataReader.GetInt32(0).ToString();
+                    @event.CustomerUserEmail = dataReader.GetString(4);
+                    @event.CustomerUserFirstName = dataReader.GetString(2);
+                    @event.CustomerUserLastName = dataReader.GetString(3);
+                    @event.EventName = "TestEventRecorded";
+                    events.Add(@event);
+                    //Console.WriteLine("\t{0}\t\t{1}", dataReader.GetString(0), dataReader.GetString(1));
+                }
+            }
+            else
+                Console.WriteLine("No rows returned.");
+
+            dataReader.Close();
+            conn.Close();
+
+            service.TrackCustomerUserEvents(events, true).ContinueWith(result =>
+            {
+                if (result.IsFaulted)
+                {
+                    Console.WriteLine(result.Exception);
+                }
+                else
+                {
+                    Console.WriteLine("The events were recorded");
+                }
+            });
             Console.ReadLine();
         }
     }
